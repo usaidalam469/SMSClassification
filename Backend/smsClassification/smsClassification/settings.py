a5bf0ad2a41fb32666c95941151cb8fffb471656
Usaid <usaidalam469@gmail.com>
2024-10-15 18:25:34 +0100

    Enaabled CORS - Backend

--- Backend/smsClassification/smsClassification/settings.py
+++ Backend/smsClassification/smsClassification/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'smsClassification.urls'
 
@@ -119,9 +122,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static", 
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
